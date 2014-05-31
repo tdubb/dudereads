81f104f322b56d674a8ab5783466da943ab649e7
twasden <wasdenonrails@gmail.com>
2014-05-31 15:41:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dudereads::Application.config.secret_key_base = 'aa7dffba81823ec3299618105ac8fc441f4aad247b46eb9d7e62e14aa1f8e2ada82e2fe6278f48930990965f381b6a15d7a507f4bb4b0c1ed6929cf2f57bd086'
